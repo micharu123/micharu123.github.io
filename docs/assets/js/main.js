fbd8249a4a526ee218d183b9b98ed6aa7d8585f5
Michael Rushanan <micharu123@gmail.com>
2021-06-29 23:07:36 -0400

    Updated search function

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,21 +1,24 @@
 "use strict";
 
+// Global so that search will actually work.
+var sjs = null;
+
 // Function to dynamically update navigation main with post title or search bar.
 function insert_into_nav_main(search_bar, post_title) {
     var ul = document.getElementsByClassName("main-nav")[0].childNodes[1];
     var li = document.createElement("li");
 
     if (search_bar) {
-        li.innerHTML = "<div id=\"search-container\"><input type=\"text\" class=\"search-input\" placeholder=\"Search on title, text, tag...\"><ul id=\"results-container\"></ul></div>";
+        li.innerHTML = "<div id=\"search-container\"><input type=\"text\" class=\"search-input\" placeholder=\"$ Type to search\" autofocus onKeyPress=\"handle_search_input(event)\" onKeyUp=\"handle_search_input(event)\">";
 
         // Insert to head of nav.
         ul.insertBefore(li, ul.firstChild);
 
         // Instantiate search object.
-        SimpleJekyllSearch({
+        sjs = SimpleJekyllSearch({
             searchInput: document.getElementById('search-input'),
             resultsContainer: document.getElementById('results-container'),
-            json: '/assets/js/search.json'
+            json: 'localhost:4000/assets/js/search.json'
         });
     }
     else {
@@ -24,4 +27,9 @@ function insert_into_nav_main(search_bar, post_title) {
         // Append the new element.
        ul.appendChild(li);
     }
+}
+
+function handle_search_input(event) {
+    const value = event.target.value;
+    sjs.search(value);
 }
